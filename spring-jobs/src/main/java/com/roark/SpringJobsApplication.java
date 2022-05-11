c0f8c7f71b2c1f8f5e7ef0c6e331260aaf369c4d
ak <arurnkumar@gmail.com>
2022-05-11 06:56:56 +0530

    added listeners

--- spring-jobs/src/main/java/com/roark/SpringJobsApplication.java
+++ spring-jobs/src/main/java/com/roark/SpringJobsApplication.java
@@ -1,18 +1,8 @@
 package com.roark;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableBatchProcessing
@@ -23,35 +13,6 @@ public class SpringJobsApplication {
 	}
 	
 
-
-	@Autowired
-	JobBuilderFactory jobs;
-	
-	
-	@Autowired
-	StepBuilderFactory steps;
-
-	
-
-	public Tasklet helloWorldTasklet() {
-		return (new Tasklet() {
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("Hello world");
-				return RepeatStatus.FINISHED;
-			}
-		});
-	}
-
-	@Bean
-	public Step step1() {
-		return steps.get("step1").tasklet(helloWorldTasklet()).build();
-	}
-
-	@Bean
-	public Job helloWorldJob() {
-		return jobs.get("helloWorldJob").start(step1()).build();
-	}
 
 
 
